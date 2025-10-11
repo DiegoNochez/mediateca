9123e53a461849caab577a99c2b4ae5f6db5d2d6
Nelsonhc22 <nelsoneliashc22@gmail.com>
2025-10-11 12:28:07 -0600

    Update ServiceFactory.java

--- src/main/java/com/diego/mediateca/app/ServiceFactory.java
+++ src/main/java/com/diego/mediateca/app/ServiceFactory.java
@@ -1,4 +1,3 @@
-// src/main/java/com/diego/mediateca/app/ServiceFactory.java
 package com.diego.mediateca.app;
 
 import com.diego.mediateca.db.MaterialDAO;
@@ -9,13 +8,9 @@ public final class ServiceFactory {
     private ServiceFactory() {}
 
     public static MaterialService materialService() {
-        // ===== OPCIÓN JDBC (recomendada si Persona 3 ya hizo BD) =====
+      
         MaterialDAO dao = new MaterialDAO();
         MaterialRepository repo = new MaterialRepository(dao);
         return new MaterialService(repo);
-
-        // ===== OPCIÓN EN MEMORIA (fallback) =====
-        // MaterialRepository repo = new MaterialRepository();
-        // return new MaterialService(repo);
     }
 }
